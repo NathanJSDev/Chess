24692c5bb71910d7d8e3d8c94d93060b7f19e518
Nathan Santos Diogo Cuteta <barbequil14@gmail.com>
2024-06-10 18:39:23 -0300

    BoardException and Defensive programing

---
+++ src/main/java/com/nd/chess/boardgame/BoardException.java
@@ -0,0 +1,10 @@
+package com.nd.chess.boardgame;
+
+public class BoardException extends RuntimeException {
+    private static final long serialVersionUID = 1l;
+
+    public BoardException(String msg){
+        super(msg);
+    }
+    
+}
